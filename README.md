3fad55047374799dda8ea0cd036c34423c784e9d
Ole Kristian Losvik <losvik@gmail.com>
2022-10-02 23:46:51 +0200

    Initialize Wagtail

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aria content hub
+
+## Development
+
+Run `poetry shell`, and get ready with
+
+- Database migration: `python manage.py migrate`
+- Add super user: `python manage.py createsuperuser`
+- Run dev server: `python manage.py runserver`
---
+++ aria/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#v1$q87))o6z-uc#*g-c&lr#4#(%$=5r+jg-e7xq&-!6d$h4sf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
