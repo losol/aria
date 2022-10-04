a82e253f54fe015d0141e20f1e91a1d087e6bc60
Ole Kristian Losvik <losvik@gmail.com>
2022-10-04 22:04:53 +0200

    Document model

---
+++ aria/documents/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from wagtail.documents.models import AbstractDocument
+from wagtail.documents.models import Document as WagtailDocument
+
+
+class StandardDocument(AbstractDocument):
+    description = models.TextField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    license = models.ForeignKey(
+        'core.LicenseSnippet',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    admin_form_fields = WagtailDocument.admin_form_fields + (
+        'description',
+        'license'
+    )
