4bfa5c527ab505dd640ed2bb4983cbe1a801d513
Ole Kristian Losvik <losvik@gmail.com>
2022-10-05 10:29:54 +0200

    Add webpack and tailwind

---
+++ aria/static_source/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['dist/*.html'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
